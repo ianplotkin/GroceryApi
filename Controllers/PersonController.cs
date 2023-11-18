94ee29bc87b99d3a29db935b0f27b08b0855b006
Ian Plotkin <19823136+ianplotkin@users.noreply.github.com>
2023-11-18 09:59:57 -0500

    Add project files.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly List<Person> people = new List<Person>();
+
+        public PersonController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            people.Add(new Person { FirstName = "Ian" });
+            people.Add(new Person { FirstName = "Annaleigh" });
+        }
+
+        [HttpGet(Name = "GetPerson")]
+        public IEnumerable<Person> Get()
+        {
+            return people.ToArray();
+        }
+    }
+}
