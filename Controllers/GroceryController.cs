44a999da5faf421fa23f58ac11b83c9dc8db89f1
Ian Plotkin <19823136+ianplotkin@users.noreply.github.com>
2023-12-02 06:56:58 -0500

    addedgroceries

---
+++ Controllers/GroceryController.cs
@@ -0,0 +1,25 @@
+﻿using GroceryApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GroceryController : ControllerBase
+    {
+        private readonly ILogger<GroceryController> _logger;
+
+        public GroceryController(ILogger<GroceryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetGrocery")]
+        public IEnumerable<Grocery> Get()
+        {
+            return null;
+        }
+
+    }
+}
